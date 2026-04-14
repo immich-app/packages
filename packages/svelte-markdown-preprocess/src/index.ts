a0a2ac7a3e739ed64800cfb1ff86928535d87590
Jason Rasmussen <jason@rasm.me>
2026-04-14 11:36:21 -0400

    feat: preprocess overhaul (#67)

--- packages/svelte-markdown-preprocess/src/index.ts
+++ packages/svelte-markdown-preprocess/src/index.ts
@@ -1,84 +1,141 @@
 import fm from 'front-matter';
-import { marked, MarkedExtension } from 'marked';
-import { mkdirSync, writeFileSync } from 'node:fs';
-import { dirname } from 'node:path';
+import { marked } from 'marked';
 import type { PreprocessorGroup } from 'svelte/compiler';
 import { markedSvelte } from './marked-svelte.js';
-import { SvelteFile } from './svelte-file.js';
 
-type Layouts = Record<string, string>;
-type Options = {
-  /** defaults to `@immich/ui` */
-  markdownPackageName?: string;
-  debugPath?: string;
-  layouts?: Layouts;
-  /** additional extensions to use with marked */
-  markedExtensions?: MarkedExtension[];
+type MaybePromise<T> = Promise<T> | T;
+
+export type FileWithContent = { filename: string; content: string };
+export type FileWithFrontMatter = { filename: string; attributes: FrontMatterAttributes; body: string };
+export type FileWithScriptBody = FileWithFrontMatter & { scriptBody: string };
+export type FileWithMarkup = FileWithScriptBody & { markup: string };
+export type FileWithLayout = FileWithMarkup & { layout?: string };
+export type FileWithSvelte = FileWithLayout & { svelte: string };
+
+export type FrontMatterAttributes = {
+  layout?: string;
+  [key: string]: unknown;
 };
 
-const resolveLayout = (layout?: string, layouts?: Layouts) => {
-  if (!layouts) {
-    return;
-  }
-
-  return layouts[layout || 'default'];
+export type SvelteMarkdownPreprocessLayouts = {
+  _?: string;
+  default?: string;
+  [key: string]: string | undefined;
 };
 
 const SCRIPT_BODY_REGEX = /<script.*>(?<body>(.|\n)*?)<\/script>/;
 
-const parse = (content: string) => {
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-expect-error
-  const { attributes, body: bodyParsed } = fm(content);
-
-  let scriptBody = '';
-  let body = bodyParsed;
-  const match = SCRIPT_BODY_REGEX.exec(body);
-  const bodyGroup = match?.groups?.body;
-  if (bodyGroup) {
-    scriptBody = bodyGroup.trim();
-    body = body.slice(Math.max(0, match.index + match[0].length)).trim();
-  }
-
-  return { attributes, body, scriptBody };
+export type SvelteMarkdownPreprocessOptions = {
+  /** defaults to `['.md', '.mdx']` */
+  extensions?: string[];
+  layouts?: SvelteMarkdownPreprocessLayouts;
 };
 
-export const svelteMarkdownPreprocess = (options: Options) => {
-  const { layouts, debugPath, markdownPackageName = '@immich/ui', markedExtensions = [] } = options || {};
+export class SvelteMarkdownPreprocess {
+  name = '@immich/svelte-markdown-preprocess';
 
-  const md = marked.use(...markedExtensions, markedSvelte());
+  #extensions: string[];
+  #layouts: SvelteMarkdownPreprocessLayouts;
+  #md: typeof marked;
 
-  return {
-    name: '@immich/svelte-markdown-preprocess',
-    async markup({ content, filename }) {
-      if (!filename?.endsWith('.md')) {
-        return;
-      }
+  constructor(options?: SvelteMarkdownPreprocessOptions) {
+    this.#extensions = options?.extensions ?? ['.md', '.mdx'];
+    this.#layouts = options?.layouts ?? {};
+    this.#md = this.configure(marked);
+  }
 
-      const { attributes, body, scriptBody } = parse(content);
-      const markup = await md.parse(body);
+  configure(md: typeof marked) {
+    return md.use(markedSvelte());
+  }
 
-      const file = new SvelteFile();
-      file.addScript(`import { Markdown } from '${markdownPackageName}';`);
+  async markup({ filename, content }: { filename?: string; content: string }) {
+    if (!filename) {
+      return;
+    }
 
-      const layout = resolveLayout(attributes.layout, layouts);
-      if (layout) {
-        file.addScript(`import Layout from '${layout}';`);
-        file.addTag(`<Layout attributes={${JSON.stringify(attributes)}}>`, '</Layout>');
-      }
+    if (!this.matches({ filename, content })) {
+      return;
+    }
 
-      file.addScript(scriptBody);
-      file.addTemplate(markup);
+    return Promise.resolve({ filename, content })
+      .then((file) => this.parseFrontMatter(file))
+      .then((file) => this.parseScriptBody(file))
+      .then((file) => this.parseMarkdown(file))
+      .then((file) => this.parseLayout(file))
+      .then((file) => this.parseSvelte(file))
+      .then((file) => ({ code: file.svelte }));
+  }
 
-      const code = file.export();
+  matches(file: FileWithContent): boolean {
+    return this.#extensions.some((extension) => file.filename.endsWith(extension));
+  }
 
-      if (debugPath) {
-        const dir = dirname(debugPath);
-        mkdirSync(dir, { recursive: true });
-        writeFileSync(debugPath, code);
-      }
+  parseFrontMatter({ filename, content }: FileWithContent): MaybePromise<FileWithFrontMatter> {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
+    const { attributes, body } = fm(content) as { attributes: FrontMatterAttributes; body: string };
+    return { filename, body, attributes };
+  }
 
-      return { code };
-    },
-  } satisfies PreprocessorGroup;
+  parseScriptBody(file: FileWithFrontMatter): MaybePromise<FileWithScriptBody> {
+    let scriptBody = '';
+    let body = file.body;
+    const match = SCRIPT_BODY_REGEX.exec(body);
+    const bodyGroup = match?.groups?.body;
+    if (bodyGroup) {
+      scriptBody = bodyGroup.trim();
+      body = body.slice(Math.max(0, match.index + match[0].length)).trim();
+    }
+
+    return { ...file, body, scriptBody };
+  }
+
+  async parseMarkdown(file: FileWithScriptBody): Promise<FileWithMarkup> {
+    const markup = await this.#md.parse(file.body);
+    return { ...file, markup };
+  }
+
+  parseLayout(file: FileWithMarkup): MaybePromise<FileWithLayout> {
+    const layoutKey = file.attributes.layout;
+    const layout = layoutKey ? this.#layouts[layoutKey] : (this.#layouts.default ?? this.#layouts._);
+    return { ...file, layout };
+  }
+
+  parseSvelte(file: FileWithLayout): MaybePromise<FileWithSvelte> {
+    const script = this.createSvelteScript(file);
+    const template = this.createSvelteTemplate(file);
+    const svelte = `${script}\n${template}`;
+    return { ...file, svelte };
+  }
+
+  scriptImports(file: FileWithLayout): Array<string | undefined> {
+    return [
+      `  import { Markdown } from '@immich/ui';`,
+      file.layout ? `  import Layout from '${file.layout}';` : undefined,
+    ];
+  }
+
+  scriptExtras(file: FileWithLayout): Array<string | undefined> {
+    return file.layout ? [`  const attributes = ${JSON.stringify(file.attributes)};`] : [];
+  }
+
+  createSvelteScript(file: FileWithLayout): string {
+    return [
+      `<script>`,
+      ...this.scriptImports(file).filter(Boolean),
+      ...this.scriptExtras(file).filter(Boolean),
+      file.scriptBody,
+      `</script>`,
+    ]
+      .filter(Boolean)
+      .join('\n');
+  }
+
+  createSvelteTemplate(file: FileWithLayout): string {
+    return (file.layout ? [`<Layout {attributes}>`, file.markup, '</Layout>'] : [file.markup]).join('\n');
+  }
+}
+
+export const svelteMarkdownPreprocess = (options?: SvelteMarkdownPreprocessOptions) => {
+  return new SvelteMarkdownPreprocess(options) satisfies PreprocessorGroup;
 };
